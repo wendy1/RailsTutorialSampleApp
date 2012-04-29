c9b6e1f190aa401513137f8ead674e8eb8b3d557
Wendy Swanbeck <olive.swanbeck@verizon.net>
2012-04-29 16:51:52 -0400

    start on security model - only logged on users can edit their own user

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -22,7 +22,11 @@ module SessionsHelper
     @current_user ||= user_from_remember_token
   end
   
-  private
+  def current_user?(user)
+    user == current_user
+  end
+  
+private
   
   def user_from_remember_token
     User.authenticate_with_salt(*remember_token)
@@ -30,6 +34,10 @@ module SessionsHelper
   
   def remember_token
     cookies.signed[:remember_token] || [nil, nil]
+  end
+  
+  def deny_access
+    redirect_to signin_path, :notice => "Please sign in to access this page"
   end
   
 end
