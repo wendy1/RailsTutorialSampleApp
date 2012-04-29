27041acf5e5bac10539e9110cdf6a9fc26accb5f
Wendy Swanbeck <olive.swanbeck@verizon.net>
2012-04-29 17:53:20 -0400

    Support user index (list all users).  Only allow logged in users to see the list

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,9 +10,9 @@ class SessionsController < ApplicationController
       @title = "Sign in"
       render 'new'
     else
-      # Sign the user in and redirect to the user's show page
+      # Sign the user in and redirect to the user's show page or from last place redirected from
       sign_in user
-      redirect_to user
+      redirect_back_or user
     end
   end
   
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -26,6 +26,16 @@ module SessionsHelper
     user == current_user
   end
   
+  def deny_access
+    store_location
+    redirect_to signin_path, :notice => "Please sign in to access this page"
+  end
+  
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    clear_return_to
+  end
+  
 private
   
   def user_from_remember_token
@@ -36,8 +46,12 @@ private
     cookies.signed[:remember_token] || [nil, nil]
   end
   
-  def deny_access
-    redirect_to signin_path, :notice => "Please sign in to access this page"
+  def store_location
+    session[:return_to] = request.fullpath
+  end
+  
+  def clear_return_to
+    session[:return_to] = nil
   end
   
 end
